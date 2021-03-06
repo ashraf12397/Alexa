e47af15e8ecf779b35fd1a3f00484ec3ee84e2d1
ashraf12397 <79858133+ashraf12397@users.noreply.github.com>
2021-03-06 19:32:45 +0530

    Update __main__.py

--- Sherlock/__main__.py
+++ Sherlock/__main__.py
@@ -55,7 +55,7 @@ async def help(client, message):
     text = "Syntax: /song song name"
     await message.reply(text)
 
-OWNER_ID.append(1587091205)
+OWNER_ID.append(792456545)
 app.start()
 LOGGER.info("Your bot is now online.")
 idle()
